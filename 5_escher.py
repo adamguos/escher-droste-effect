bbf3148fe29610af5a3852b9c6f58a1cc9b9311d
Adam Guo <agsmguo@gmail.com>
2021-05-20 21:06:10 +0800

    Fix blending and tiling

--- 5_escher.py
+++ 5_escher.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pdb
+import scipy.signal
 import sys
 
 
@@ -18,6 +19,7 @@ def escher(img_path):
 
     zt = transform(z, r1, r2)
     new_img = change_coords(img, zt)
+    new_img = blend_zeros(new_img)
 
     filename = f"{'.'.join(img_path.split('.')[:-1])}_escher.{img_path.split('.')[-1]}"
     cv2.imwrite(filename, new_img)
@@ -31,10 +33,16 @@ def transform(z, r1, r2):
     annulus = np.where((np.abs(z) < r2) & (np.abs(z) > r1), np.log(z / r1), 0)
     annulus *= beta
 
-    h_disp = np.log(r2 / r1) + np.sin(alpha) * np.log(r2 / r1) * 1j
+    h_disp = np.log(r2 / r1) - (np.tan(alpha) * np.log(
+        r2 / r1) * np.sin(alpha)) + (1j * np.sin(alpha) * np.log(r2 / r1))
     v_disp = -2j * np.pi
-    annulus = np.concatenate((annulus, annulus + h_disp, annulus + 2 * h_disp))
-    annulus = np.concatenate((annulus, annulus + v_disp))
+    annulus = np.concatenate((
+        annulus - h_disp,
+        annulus,
+        annulus + h_disp,
+        annulus + 2 * h_disp,
+    ))
+    # annulus = np.concatenate((annulus, annulus + v_disp))
 
     annulus = np.exp(annulus)
 
@@ -64,6 +72,32 @@ def change_coords(img, new_coords):
             new_img[y_new[i, j], x_new[i, j]] = img[i % img.shape[0], j]
 
     return new_img
+
+
+def blend_zeros(arr):
+    new_arr = np.copy(arr).astype(int)
+    zero = np.nonzero(np.all(arr == [0, 0, 0], axis=-1))
+
+    r = (np.mod(zero[0] + 1, arr.shape[0]), zero[1])
+    l = (np.mod(zero[0] - 1, arr.shape[0]), zero[1])
+    u = (zero[0], zero[1] - 1)
+    d = (zero[0], np.mod(zero[1] + 1, arr.shape[1]))
+    ur = (np.mod(zero[0] + 1, arr.shape[0]), zero[1] - 1)
+    ul = (zero[0] - 1, zero[1] - 1)
+    dr = (np.mod(zero[0] + 1, arr.shape[0]), np.mod(zero[1] + 1, arr.shape[1]))
+    dl = (zero[0] - 1, np.mod(zero[1] + 1, arr.shape[1]))
+
+    new_arr[zero] += arr[r]
+    new_arr[zero] += arr[l]
+    new_arr[zero] += arr[u]
+    new_arr[zero] += arr[d]
+    new_arr[zero] += arr[ur]
+    new_arr[zero] += arr[ul]
+    new_arr[zero] += arr[dr]
+    new_arr[zero] += arr[dl]
+    new_arr[zero] = new_arr[zero] / 8
+
+    return new_arr.astype("uint8")
 
 
 if __name__ == "__main__":
